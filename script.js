f78974174345c8472da85588285962eee09ba418
alex <alexwojtak@hotmail.com>
2021-03-05 16:48:21 +0000

    Add condition to lose by falling in hole

--- script.js
+++ script.js
@@ -15,16 +15,27 @@ window.addEventListener("keydown", function(e) {
 
 //Check win condition
 function checkWin() {
-	console.log("Position x is " + playerx + " and position y is " + playery);
+	//console.log("Position x is " + playerx + " and position y is " + playery);
 	if (playerx === doorx && playery === doory) {
 		console.log("win");
 		document.getElementById("winMessage").style.display=("inline");
 	}
 }
 
+function checkLoss() {
+	if(myField.level[playery][playerx] === 1){
+		document.getElementById("loseMessage").style.display=("inline");
+		console.log("lose");
+	}
+}
+
 class Field {
 	constructor (level){
 		this._level = level;
+	}
+	
+	get level(){
+		return this._level;
 	}
 
 	//Print Field method creates full HTML string to add via innerHTML. 
@@ -35,24 +46,34 @@ class Field {
 		let tempString = "";
 		let tempString2 = "";
 		let tempString3 = "";
+		let sprite = "";
 		for (i=0; i < this._level.length; i++){
 			tempString2 = "";
 			for (j=0; j < this._level[0].length; j++){
-				tempString = "<img id='x" + i + "y" + j + "' class='BGTile' style='top:" + 24 * i + "px; left:" + 24 * j + "px' src='pack/rpg-pack/props n decorations/generic-rpg-barrel03.png'>"
+				//Create the html string and positions each sprite
+				if (this._level[i][j] === 0){
+					sprite = "pack/rpg-pack/props n decorations/generic-rpg-barrel03.png";
+					console.log("location " + i + ", " + j + " is a barrel.");
+				}
+				else if(this._level[i][j] === 1){
+					sprite = "pack/rpg-pack/props n decorations/hole.png";
+					console.log("Not a barrel");
+				}
+				tempString = "<img id='x" + i + "y" + j + "' class='BGTile' style='top:" + ((24 * i)+5) + "px; left:" + ((24 * j)+5) + "px' src='" + sprite + "'>"
 				//console.log(tempString);
 				tempString2 += tempString;
 			}
-			console.log(tempString2);
+			//console.log(tempString2);
 			tempString3 += tempString2;
 		}	
-		console.log("Big string is " + tempString3);
+		//console.log("Big string is " + tempString3);
 		document.getElementById('gamearea').innerHTML += tempString3;
 	}
 }
 
-const myField = new Field([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+const myField = new Field([[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -79,6 +100,7 @@ function checkKey(e) {
 		//Move player 24px (player dimension) in the desired direction
 		document.getElementById("playerChar").style.top=((24 * playery) + 'px');
 		checkWin();
+		checkLoss();
 	}
     }
     else if (e.keyCode == 40 || e.keyCode === 83) {
@@ -86,6 +108,7 @@ function checkKey(e) {
         	playery++;
 		document.getElementById("playerChar").style.top=((24 * playery) + 'px');
 		checkWin();
+		checkLoss();
         }
     }
     else if (e.keyCode == 37 || e.keyCode === 65) {
@@ -94,6 +117,7 @@ function checkKey(e) {
 	    	playerx--;
 		document.getElementById("playerChar").style.left=((24 * playerx) + 'px');
 		checkWin();
+		checkLoss();
 	}
     }
     else if (e.keyCode == 39 || e.keyCode === 68) {
@@ -102,6 +126,7 @@ function checkKey(e) {
 	    	playerx++;
 		document.getElementById("playerChar").style.left=((24 * playerx) + 'px');
 		checkWin();
+		checkLoss();
 	}
     }
 
